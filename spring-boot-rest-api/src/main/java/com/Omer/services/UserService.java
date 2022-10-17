483538455cfbd78f33a3b6c18f284caee3e212b1
OMER FARUK GUZEL <58605364+omerfarukgzl@users.noreply.github.com>
2022-10-17 16:42:18 +0300

    update params

--- spring-boot-rest-api/src/main/java/com/Omer/services/UserService.java
+++ spring-boot-rest-api/src/main/java/com/Omer/services/UserService.java
@@ -6,7 +6,7 @@ import com.Omer.entity.User;
 import java.util.List;
 
 public interface UserService {
-     UserDto createUser(UserDto user);
+     UserDto createUser(UserDto userDto);
      List<UserDto> getUsers();
      UserDto getUser(Long id);
      UserDto updateUser(Long id,UserDto userDto);
